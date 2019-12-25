ac5a682e301e5eb15acce73d4d16276ff150578f
Adam Nemecek <adamnemecek@gmail.com>
2019-12-25 08:13:43 -0800

    refactored midi

--- src/lib_midi/src/lib.rs
+++ src/lib_midi/src/lib.rs
@@ -15,79 +15,75 @@ pub struct Midi {
 
 impl Midi {
     pub fn new(path: &str) -> Result<Self, String> {
-        read_file(path)
-    }
-}
+        let smf_buffer = match midly::SmfBuffer::open(path) {
+            Ok(buff) => buff,
+            Err(_) => return Err(String::from("Could Not Open File")),
+        };
 
+        let smf = match smf_buffer.parse_collect() {
+            Ok(smf) => smf,
+            Err(_) => return Err(String::from("Midi Parsing Error (midly lib)")),
+        };
 
-fn read_file(path: &str) -> Result<Midi, String> {
-    let smf_buffer = match midly::SmfBuffer::open(path) {
-        Ok(buff) => buff,
-        Err(_) => return Err(String::from("Could Not Open File")),
-    };
+        let u_per_quarter_note: u16;
 
-    let smf = match smf_buffer.parse_collect() {
-        Ok(smf) => smf,
-        Err(_) => return Err(String::from("Midi Parsing Error (midly lib)")),
-    };
+        match smf.header.timing {
+            midly::Timing::Metrical(t) => u_per_quarter_note = t.as_int(),
+            midly::Timing::Timecode(_fps, _u) => {
+                return Err(String::from("Midi With Timecode Timing, Not Supported!"));
+            }
+        };
 
-    let u_per_quarter_note: u16;
-
-    match smf.header.timing {
-        midly::Timing::Metrical(t) => u_per_quarter_note = t.as_int(),
-        midly::Timing::Timecode(_fps, _u) => {
-            return Err(String::from("Midi With Timecode Timing, Not Supported!"));
+        if smf.tracks.is_empty() {
+            return Err(String::from("Midi File Has No Tracks"));
         }
-    };
 
-    if smf.tracks.is_empty() {
-        return Err(String::from("Midi File Has No Tracks"));
-    }
-
-    let mut tracks: Vec<MidiTrack> = Vec::new();
-    for (i, trk) in smf.tracks.iter().enumerate() {
-        tracks.push(MidiTrack::new(trk, i));
-    }
-
-    let tp = &mut tracks_parser::TracksParser::new(u_per_quarter_note);
-
-    use midly::Format;
-    match smf.header.format {
-        Format::SingleTrack => {
-            tp.parse(&mut tracks, &smf.tracks);
+        let mut tracks: Vec<MidiTrack> = Vec::new();
+        for (i, trk) in smf.tracks.iter().enumerate() {
+            tracks.push(MidiTrack::new(trk, i));
         }
-        Format::Parallel => {
-            tp.parse(&mut tracks, &smf.tracks);
-        }
-        Format::Sequential => {
-            return Err(String::from("MultiSong Midi Not Supported"));
-        }
-    }
 
-    let mut merged_track: MidiTrack = tracks[0].clone();
+        let tp = &mut tracks_parser::TracksParser::new(u_per_quarter_note);
 
-    for (i, trk) in tracks.iter().enumerate() {
-        if i > 0 {
-            for n in trk.notes.iter() {
-                let n = n.clone();
-                merged_track.notes.push(n);
+        use midly::Format;
+        match smf.header.format {
+            Format::SingleTrack => {
+                tp.parse(&mut tracks, &smf.tracks);
+            }
+            Format::Parallel => {
+                tp.parse(&mut tracks, &smf.tracks);
+            }
+            Format::Sequential => {
+                return Err(String::from("MultiSong Midi Not Supported"));
             }
         }
+
+        let mut merged_track: MidiTrack = tracks[0].clone();
+
+        for (i, trk) in tracks.iter().enumerate() {
+            if i > 0 {
+                for n in trk.notes.iter() {
+                    let n = n.clone();
+                    merged_track.notes.push(n);
+                }
+            }
+        }
+
+        merged_track
+            .notes
+            .sort_by(|a, b| a.start.partial_cmp(&b.start).unwrap());
+
+        // Asign Unique Id
+        for (i, note) in merged_track.notes.iter_mut().enumerate() {
+            note.id = i;
+        }
+
+        Ok(Self {
+            // tracks_count: tracks.len() as u16,
+            format: smf.header.format,
+            tracks,
+            merged_track,
+        })
     }
-
-    merged_track
-        .notes
-        .sort_by(|a, b| a.start.partial_cmp(&b.start).unwrap());
-
-    // Asign Unique Id
-    for (i, note) in merged_track.notes.iter_mut().enumerate() {
-        note.id = i;
-    }
-
-    Ok(Midi {
-        // tracks_count: tracks.len() as u16,
-        format: smf.header.format,
-        tracks,
-        merged_track,
-    })
 }
+

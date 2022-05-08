d422d6658999fa9dfc8ab6598922e16837f84d96
Poly <marynczakbartlomiej@gmail.com>
2022-05-08 16:36:18 +0200

    recorder: Use rgba instead of bgra

--- src/bin/neothesia-cli.rs
+++ src/bin/neothesia-cli.rs
@@ -155,7 +155,7 @@ fn main() {
                 let mapping = slice.get_mapped_range();
 
                 let data: &[u8] = &mapping;
-                encoder.encode_bgra(1920, 1080, data, false);
+                encoder.encode_rgba(1920, 1080, data, false);
                 print!(
                     "\r Encoded {} frames ({}s, {}%) in {}s",
                     n,

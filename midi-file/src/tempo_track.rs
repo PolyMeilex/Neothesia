559dcf7d755868c981f127724ac35a15c984a1a5
Bartłomiej Maryńczak <marynczakbartlomiej@gmail.com>
2023-09-17 22:37:14 +0200

    Move tempo track out of utils (#57)

--- midi-file/src/tempo_track.rs
+++ midi-file/src/tempo_track.rs
@@ -2,60 +2,6 @@ use crate::TempoEvent;
 use midly::{MetaMessage, TrackEvent, TrackEventKind};
 use std::{collections::HashMap, time::Duration};
 
-fn pulse_to_duration(pulses: u64, tempo: u32, pulses_per_quarter_note: u16) -> Duration {
-    let u_time = pulses as f64 / pulses_per_quarter_note as f64;
-    // We floor only because Synthesia floors,
-    // so if we want to test for timing regresions we have to do the same
-    let time = (u_time * tempo as f64).floor() as u64;
-    Duration::from_micros(time)
-}
-
-pub fn pulses_to_duration(
-    tempo_events: &[TempoEvent],
-    event_pulses: u64,
-    pulses_per_quarter_note: u16,
-) -> Duration {
-    let mut res = Duration::ZERO;
-
-    let mut hit = false;
-    let mut last_tempo_event_pulses = 0u64;
-    let mut running_tempo = 500_000;
-
-    let event_pulses = event_pulses;
-
-    for tempo_event in tempo_events.iter() {
-        let tempo_event_pulses = tempo_event.absolute_pulses;
-
-        // If the time we're asking to convert is still beyond
-        // this tempo event, just add the last time slice (at
-        // the previous tempo) to the running wall-clock time.
-        let delta_pulses = if event_pulses > tempo_event_pulses {
-            tempo_event_pulses - last_tempo_event_pulses
-        } else {
-            hit = true;
-            event_pulses - last_tempo_event_pulses
-        };
-
-        res += pulse_to_duration(delta_pulses, running_tempo, pulses_per_quarter_note);
-
-        // If the time we're calculating is before the tempo event we're
-        // looking at, we're done.
-        if hit {
-            break;
-        }
-
-        running_tempo = tempo_event.tempo;
-        last_tempo_event_pulses = tempo_event_pulses;
-    }
-
-    if !hit {
-        let remaining_pulses = event_pulses - last_tempo_event_pulses;
-        res += pulse_to_duration(remaining_pulses, running_tempo, pulses_per_quarter_note);
-    }
-
-    res
-}
-
 pub struct TempoTrack(Vec<TempoEvent>);
 
 impl std::ops::Deref for TempoTrack {
@@ -110,4 +56,54 @@ impl TempoTrack {
 
         TempoTrack(tempo_events)
     }
+
+    pub fn pulses_to_duration(&self, event_pulses: u64, pulses_per_quarter_note: u16) -> Duration {
+        let mut res = Duration::ZERO;
+
+        let mut hit = false;
+        let mut last_tempo_event_pulses = 0u64;
+        let mut running_tempo = 500_000;
+
+        let event_pulses = event_pulses;
+
+        for tempo_event in self.0.iter() {
+            let tempo_event_pulses = tempo_event.absolute_pulses;
+
+            // If the time we're asking to convert is still beyond
+            // this tempo event, just add the last time slice (at
+            // the previous tempo) to the running wall-clock time.
+            let delta_pulses = if event_pulses > tempo_event_pulses {
+                tempo_event_pulses - last_tempo_event_pulses
+            } else {
+                hit = true;
+                event_pulses - last_tempo_event_pulses
+            };
+
+            res += pulse_to_duration(delta_pulses, running_tempo, pulses_per_quarter_note);
+
+            // If the time we're calculating is before the tempo event we're
+            // looking at, we're done.
+            if hit {
+                break;
+            }
+
+            running_tempo = tempo_event.tempo;
+            last_tempo_event_pulses = tempo_event_pulses;
+        }
+
+        if !hit {
+            let remaining_pulses = event_pulses - last_tempo_event_pulses;
+            res += pulse_to_duration(remaining_pulses, running_tempo, pulses_per_quarter_note);
+        }
+
+        res
+    }
+}
+
+fn pulse_to_duration(pulses: u64, tempo: u32, pulses_per_quarter_note: u16) -> Duration {
+    let u_time = pulses as f64 / pulses_per_quarter_note as f64;
+    // We floor only because Synthesia floors,
+    // so if we want to test for timing regresions we have to do the same
+    let time = (u_time * tempo as f64).floor() as u64;
+    Duration::from_micros(time)
 }

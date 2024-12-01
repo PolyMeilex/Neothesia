e9040be00f566bc742f42d7a3e05a87927e77b8b
guylamar2006 <guychronister@outlook.com>
2024-12-01 09:42:34 -0600

    Update piano_range.rs

--- neothesia-iced-widgets/src/piano_range.rs
+++ neothesia-iced-widgets/src/piano_range.rs
@@ -5,9 +5,6 @@ use iced_core::{
     text::Renderer as TextRenderer,
     Text
 };
-// Removed unused imports
-// use iced_wgpu::Renderer;
-// use super::Element;
 
 pub struct PianoRange(pub std::ops::RangeInclusive<u8>);
 

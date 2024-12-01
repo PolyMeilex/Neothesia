4073103973d001287bb075fcd767e080932d3622
guylamar2006 <guychronister@outlook.com>
2024-12-01 13:06:30 -0600

    Update instance_data.rs

--- neothesia-core/src/render/waterfall/pipeline/instance_data.rs
+++ neothesia-core/src/render/waterfall/pipeline/instance_data.rs
@@ -1,5 +1,4 @@
 use wgpu_jumpstart::wgpu;
-
 use bytemuck::{Pod, Zeroable};
 use wgpu::vertex_attr_array;
 
@@ -10,11 +9,18 @@ pub struct NoteInstance {
     pub size: [f32; 2],
     pub color: [f32; 3],
     pub radius: f32,
+    pub note_name: [u8; 4], // Add this line
 }
 
 impl NoteInstance {
-    pub fn attributes() -> [wgpu::VertexAttribute; 4] {
-        vertex_attr_array!(1 => Float32x2, 2 => Float32x2, 3 => Float32x3, 4 => Float32)
+    pub fn attributes() -> [wgpu::VertexAttribute; 5] { // Update number of attributes
+        vertex_attr_array!(
+            1 => Float32x2, 
+            2 => Float32x2, 
+            3 => Float32x3, 
+            4 => Float32, 
+            5 => Uint32x4 // Add this line for note_name
+        )
     }
 
     pub fn layout(attributes: &[wgpu::VertexAttribute]) -> wgpu::VertexBufferLayout {

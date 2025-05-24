d154010a554bfd815ad8b25678bb3505fb106bed
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-05-24 21:58:38 +0200

    Cache note labels

--- neothesia-core/src/render/note_labels/mod.rs
+++ neothesia-core/src/render/note_labels/mod.rs
@@ -5,8 +5,66 @@ use super::KeyboardRenderer;
 use super::TextRenderer;
 use super::TextRendererInstance;
 
+#[derive(Default)]
+struct LabelsCache {
+    labels: Option<[glyphon::Buffer; 12]>,
+    neutral_width: f32,
+}
+
+impl LabelsCache {
+    fn get(
+        &mut self,
+        keyboard: &KeyboardRenderer,
+        font_system: &mut glyphon::FontSystem,
+    ) -> &[glyphon::Buffer; 12] {
+        let sharp_width = keyboard.layout().sizing.sharp_width;
+        let neutral_width = keyboard.layout().sizing.neutral_width;
+
+        if self.labels.is_none() || self.neutral_width != neutral_width {
+            let label_width = sharp_width;
+
+            let labels = [
+                ("C", neutral_width),
+                ("C#", sharp_width),
+                ("D", neutral_width),
+                ("D#", sharp_width),
+                ("E", neutral_width),
+                ("F", neutral_width),
+                ("F#", sharp_width),
+                ("G", neutral_width),
+                ("G#", sharp_width),
+                ("A", neutral_width),
+                ("A#", sharp_width),
+                ("B", neutral_width),
+            ]
+            .map(|(label, note_width)| {
+                let mut buffer = glyphon::Buffer::new(
+                    font_system,
+                    glyphon::Metrics::new(label_width, label_width),
+                );
+                buffer.set_size(font_system, Some(note_width), None);
+                buffer.set_wrap(font_system, glyphon::Wrap::None);
+                buffer.set_text(
+                    font_system,
+                    label,
+                    glyphon::Attrs::new().family(glyphon::Family::SansSerif),
+                    glyphon::Shaping::Basic,
+                );
+                buffer.lines[0].set_align(Some(glyphon::cosmic_text::Align::Center));
+                buffer.shape_until_scroll(font_system, false);
+                buffer
+            });
+
+            self.labels = Some(labels);
+        }
+
+        self.labels.as_ref().unwrap()
+    }
+}
+
 pub struct NoteLabels {
     notes: Box<[MidiNote]>,
+    labels_cache: LabelsCache,
 }
 
 impl NoteLabels {
@@ -21,48 +79,8 @@ impl NoteLabels {
 
         Self {
             notes: notes.into(),
+            labels_cache: LabelsCache::default(),
         }
-    }
-
-    #[profiling::function]
-    fn build_labels(
-        &self,
-        keyboard: &KeyboardRenderer,
-        font_system: &mut glyphon::FontSystem,
-    ) -> [glyphon::Buffer; 12] {
-        let sharp_width = keyboard.layout().sizing.sharp_width;
-        let neutral_width = keyboard.layout().sizing.neutral_width;
-        let label_width = sharp_width;
-
-        [
-            ("C", neutral_width),
-            ("C#", sharp_width),
-            ("D", neutral_width),
-            ("D#", sharp_width),
-            ("E", neutral_width),
-            ("F", neutral_width),
-            ("F#", sharp_width),
-            ("G", neutral_width),
-            ("G#", sharp_width),
-            ("A", neutral_width),
-            ("A#", sharp_width),
-            ("B", neutral_width),
-        ]
-        .map(|(label, note_width)| {
-            let mut buffer =
-                glyphon::Buffer::new(font_system, glyphon::Metrics::new(label_width, label_width));
-            buffer.set_size(font_system, Some(note_width), None);
-            buffer.set_wrap(font_system, glyphon::Wrap::None);
-            buffer.set_text(
-                font_system,
-                label,
-                glyphon::Attrs::new().family(glyphon::Family::SansSerif),
-                glyphon::Shaping::Basic,
-            );
-            buffer.lines[0].set_align(Some(glyphon::cosmic_text::Align::Center));
-            buffer.shape_until_scroll(font_system, false);
-            buffer
-        })
     }
 
     #[profiling::function]
@@ -79,7 +97,7 @@ impl NoteLabels {
         let layout = keyboard.layout();
         let label_width = layout.sizing.sharp_width;
 
-        let labels = self.build_labels(keyboard, text.font_system());
+        let labels = self.labels_cache.get(keyboard, text.font_system());
 
         for note in self.notes.iter() {
             let x = layout.keys[note.note as usize - range_start].x();

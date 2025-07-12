7001ebb420de9e195a494518b4e148c5f2887368
Bartłomiej Maryńczak <marynczakbartlomiej@gmail.com>
2025-07-12 18:49:25 +0200

    Wgpu 26 update (#265)

--- iced-wgpu/src/triangle.rs
+++ iced-wgpu/src/triangle.rs
@@ -335,6 +335,7 @@ fn render<'a>(
                         load: wgpu::LoadOp::Load,
                         store: wgpu::StoreOp::Store,
                     },
+                    depth_slice: None,
                 })],
                 depth_stencil_attachment: None,
                 timestamp_writes: None,
--- neothesia-cli/src/main.rs
+++ neothesia-cli/src/main.rs
@@ -184,6 +184,7 @@ impl Recorder {
                             load: wgpu::LoadOp::Clear(bg_color),
                             store: wgpu::StoreOp::Store,
                         },
+                        depth_slice: None,
                     })],
                     depth_stencil_attachment: None,
                     timestamp_writes: None,

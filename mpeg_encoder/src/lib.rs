bb6c8091b5217e349edee7cf15c75ca1efe4bc2e
PolyMeilex <marynczakbartlomiej@gmail.com>
2025-07-20 05:18:41 +0200

    WIP

--- mpeg_encoder/src/lib.rs
+++ mpeg_encoder/src/lib.rs
@@ -3,16 +3,21 @@
 
 // Inspired by the muxing sample: http://ffmpeg.org/doxygen/trunk/muxing_8c-source.html
 
-use ffmpeg_sys as ffmpeg;
+use ffmpeg_sys::{
+    self as ffmpeg, AVChannelLayout__bindgen_ty_1, AVChannelOrder, AVOutputFormat, AVSampleFormat,
+    SwrContext, AV_CH_LAYOUT_STEREO, AV_CODEC_CAP_VARIABLE_FRAME_SIZE,
+};
 
 use ffmpeg::{
     AVChannelLayout, AVCodec, AVCodecContext, AVCodecID, AVFormatContext, AVFrame, AVPacket,
     AVPixelFormat, AVRational, AVStream, SwsContext, AV_CODEC_FLAG_GLOBAL_HEADER,
 };
-use std::ffi::CString;
+use std::ffi::{c_void, CStr, CString};
 use std::mem;
 use std::path::Path;
 use std::ptr::{self, NonNull};
+
+pub mod new;
 
 #[derive(PartialEq)]
 enum ColorFormat {
@@ -119,6 +124,26 @@ fn init_context(
     }
 }
 
+struct VideoOutputStream {
+    stream: *mut AVStream,
+    codec_context: *mut AVCodecContext,
+    tmp_pkt: *mut AVPacket,
+
+    frame: NonNull<AVFrame>,
+    tmp_frame: Option<NonNull<AVFrame>>,
+}
+
+struct AudioOutputStream {
+    stream: *mut AVStream,
+    codec_context: *mut AVCodecContext,
+    tmp_pkt: *mut AVPacket,
+
+    frame: NonNull<AVFrame>,
+    tmp_frame: NonNull<AVFrame>,
+
+    swr_ctx: *mut SwrContext,
+}
+
 struct AudioCtx {
     stream: NonNull<AVStream>,
     context: NonNull<AVCodecContext>,
@@ -148,6 +173,358 @@ pub struct Encoder {
 }
 
 impl Encoder {
+    fn new_output_context(path: &CStr) -> NonNull<AVFormatContext> {
+        let mut output_context = ptr::null_mut();
+
+        unsafe {
+            ffmpeg::avformat_alloc_output_context2(
+                &mut output_context,
+                ptr::null_mut(),
+                ptr::null(),
+                path.as_ptr(),
+            );
+
+            NonNull::new(output_context)
+                .or_else(|| {
+                    ffmpeg::avformat_alloc_output_context2(
+                        &mut output_context,
+                        ptr::null_mut(),
+                        ptr::null(),
+                        c"mpeg".as_ptr(),
+                    );
+                    NonNull::new(output_context)
+                })
+                .expect("Unable to create the output context.")
+        }
+    }
+
+    fn new_stream(format_context: NonNull<AVFormatContext>) -> Option<NonNull<AVStream>> {
+        unsafe {
+            let stream = ffmpeg::avformat_new_stream(format_context.as_ptr(), ptr::null_mut());
+            let stream = NonNull::new(stream)?;
+
+            (*stream.as_ptr()).id = ((*format_context.as_ptr()).nb_streams - 1) as i32;
+
+            Some(stream)
+        }
+    }
+
+    fn new_video_streams(
+        format_context: NonNull<AVFormatContext>,
+        output_format: *const AVOutputFormat,
+    ) -> VideoOutputStream {
+        let codec_id = unsafe { (*output_format).video_codec };
+        assert_ne!(
+            codec_id,
+            AVCodecID::AV_CODEC_ID_NONE,
+            "The selected output container does not support video encoding"
+        );
+
+        let codec = unsafe { ffmpeg::avcodec_find_encoder(codec_id) };
+        assert!(!codec.is_null(), "Could not find video encoder");
+        let tmp_pkt = unsafe { ffmpeg::av_packet_alloc() };
+        assert!(!codec.is_null(), "Could not allocate AVPacket");
+
+        let stream = Self::new_stream(format_context)
+            .expect("Could not allocate stream")
+            .as_ptr();
+
+        let codec_context = unsafe { ffmpeg::avcodec_alloc_context3(codec) };
+        assert!(
+            !codec_context.is_null(),
+            "Could not alloc an encoding context"
+        );
+
+        unsafe {
+            (*codec_context).codec_id = codec_id;
+            (*codec_context).bit_rate = 400000;
+
+            // Resolution must be a multiple of two.
+            (*codec_context).width = 1920;
+            (*codec_context).height = 1080;
+
+            const FRAME_RATE: i32 = 60;
+            const STREAM_PIX_FMT: AVPixelFormat = AVPixelFormat::AV_PIX_FMT_YUV420P;
+
+            // timebase: This is the fundamental unit of time (in seconds) in terms
+            // of which frame timestamps are represented. For fixed-fps content,
+            // timebase should be 1/framerate and timestamp increments should be
+            // identical to 1.
+            let time_base = AVRational {
+                num: 1,
+                den: FRAME_RATE,
+            };
+            (*stream).time_base = time_base;
+            (*codec_context).time_base = time_base;
+
+            (*codec_context).gop_size = 12; // emit one intra frame every twelve frames at most
+            (*codec_context).pix_fmt = STREAM_PIX_FMT;
+
+            if (*codec_context).codec_id == AVCodecID::AV_CODEC_ID_MPEG2VIDEO {
+                // just for testing, we also add B-frames
+                // (*video_codec_context).mb_decision = 2;
+            }
+
+            if (*codec_context).codec_id == AVCodecID::AV_CODEC_ID_MPEG1VIDEO {
+                // Needed to avoid using macroblocks in which some coeffs overflow.
+                // This does not happen with normal video, it just happens here as
+                // the motion of the chroma plane does not match the luma plane.
+                (*codec_context).mb_decision = 2;
+            }
+
+            // Some formats want stream headers to be separate.
+            if (*output_format).flags & ffmpeg::AVFMT_GLOBALHEADER != 0 {
+                (*codec_context).flags |= ffmpeg::AV_CODEC_FLAG_GLOBAL_HEADER as i32;
+            }
+
+            if ffmpeg::avcodec_open2(codec_context, codec, ptr::null_mut()) < 0 {
+                panic!("Could not open video codec.");
+            }
+
+            let video_frame =
+                NonNull::new(ffmpeg::av_frame_alloc()).expect("Could not allocate video frame.");
+
+            {
+                (*video_frame.as_ptr()).format = (*codec_context).pix_fmt as i32;
+                (*video_frame.as_ptr()).width = (*codec_context).width;
+                (*video_frame.as_ptr()).height = (*codec_context).height;
+
+                /* allocate the buffers for the frame data */
+                if ffmpeg::av_frame_get_buffer(video_frame.as_ptr(), 0) < 0 {
+                    panic!("Could not allocate frame data.");
+                }
+            }
+
+            // If the output format is not YUV420P, then a temporary YUV420P
+            // picture is needed too. It is then converted to the required
+            // output format.
+            let tmp_frame = if (*codec_context).pix_fmt != AVPixelFormat::AV_PIX_FMT_YUV420P {
+                let tmp_frame = NonNull::new(ffmpeg::av_frame_alloc())
+                    .expect("Could not allocate video frame.");
+
+                (*tmp_frame.as_ptr()).format = AVPixelFormat::AV_PIX_FMT_YUV420P as i32;
+                (*tmp_frame.as_ptr()).width = (*codec_context).width;
+                (*tmp_frame.as_ptr()).height = (*codec_context).height;
+
+                /* allocate the buffers for the frame data */
+                if ffmpeg::av_frame_get_buffer(tmp_frame.as_ptr(), 0) < 0 {
+                    panic!("Could not allocate frame data.");
+                }
+
+                Some(tmp_frame)
+            } else {
+                None
+            };
+
+            // copy the stream parameters to the muxer
+            if ffmpeg::avcodec_parameters_from_context((*stream).codecpar, codec_context) < 0 {
+                panic!("Could not copy the stream parameters");
+            }
+
+            VideoOutputStream {
+                stream,
+                codec_context,
+                tmp_pkt,
+                frame: video_frame,
+                tmp_frame,
+            }
+        }
+    }
+
+    fn new_audio_streams(
+        format_context: NonNull<AVFormatContext>,
+        output_format: *const AVOutputFormat,
+    ) -> AudioOutputStream {
+        let codec_id = unsafe { (*output_format).audio_codec };
+        assert_ne!(
+            codec_id,
+            AVCodecID::AV_CODEC_ID_NONE,
+            "The selected output container does not support audio encoding"
+        );
+
+        let codec = unsafe { ffmpeg::avcodec_find_encoder(codec_id) };
+        assert!(!codec.is_null(), "Could not find audio encoder");
+
+        let tmp_pkt = unsafe { ffmpeg::av_packet_alloc() };
+        assert!(!codec.is_null(), "Could not allocate AVPacket");
+
+        let stream = Self::new_stream(format_context).expect("Could not allocate stream");
+        let codec_context = unsafe { ffmpeg::avcodec_alloc_context3(codec) };
+        assert!(
+            !codec_context.is_null(),
+            "Could not alloc an encoding context"
+        );
+
+        unsafe {
+            let sample_fmts = (*codec).sample_fmts;
+
+            (*codec_context).sample_fmt = if sample_fmts.is_null() {
+                AVSampleFormat::AV_SAMPLE_FMT_FLTP
+            } else {
+                *(*codec).sample_fmts
+            };
+
+            (*codec_context).bit_rate = 64000;
+            (*codec_context).sample_rate = 44100;
+
+            let supported_samplerates = (*codec).supported_samplerates;
+
+            if !supported_samplerates.is_null() {
+                (*codec_context).sample_rate = *supported_samplerates.offset(0);
+                let mut i = 0;
+                while *supported_samplerates.offset(i) != 0 {
+                    if *supported_samplerates.offset(i) == 44100 {
+                        (*codec_context).sample_rate = 44100;
+                    }
+                    i += 1;
+                }
+            }
+
+            let stereo_layout = AVChannelLayout {
+                order: AVChannelOrder::AV_CHANNEL_ORDER_NATIVE,
+                nb_channels: 2, // stereo
+                u: AVChannelLayout__bindgen_ty_1 {
+                    mask: AV_CH_LAYOUT_STEREO,
+                },
+                opaque: ptr::null_mut(),
+            };
+            ffmpeg::av_channel_layout_copy(&mut (*codec_context).ch_layout, &stereo_layout);
+
+            (*stream.as_ptr()).time_base = AVRational {
+                num: 1,
+                den: (*codec_context).sample_rate,
+            };
+
+            // Some formats want stream headers to be separate.
+            if (*output_format).flags & ffmpeg::AVFMT_GLOBALHEADER != 0 {
+                (*codec_context).flags |= ffmpeg::AV_CODEC_FLAG_GLOBAL_HEADER as i32;
+            }
+
+            if ffmpeg::avcodec_open2(codec_context, codec, ptr::null_mut()) < 0 {
+                panic!("Could not open audio codec.");
+            }
+
+            let nb_samples = if (*codec).capabilities & AV_CODEC_CAP_VARIABLE_FRAME_SIZE as i32 != 0
+            {
+                10000
+            } else {
+                (*codec_context).frame_size
+            };
+
+            // TODO: Make sure this is right
+            unsafe fn alloc_audio_frame(
+                sample_fmt: AVSampleFormat,
+                channel_layout: *const AVChannelLayout,
+                sample_rate: i32,
+                nb_samples: i32,
+            ) -> NonNull<AVFrame> {
+                let frame = ffmpeg::av_frame_alloc();
+                if frame.is_null() {
+                    panic!("Error allocating an audio frame");
+                }
+                (*frame).format = sample_fmt as i32;
+                ffmpeg::av_channel_layout_copy(&mut (*frame).ch_layout, channel_layout);
+                (*frame).sample_rate = sample_rate;
+                (*frame).nb_samples = nb_samples;
+
+                if nb_samples > 0 && ffmpeg::av_frame_get_buffer(frame, 0) < 0 {
+                    panic!("Error allocating an audio buffer");
+                }
+
+                NonNull::new_unchecked(frame)
+            }
+
+            let frame = alloc_audio_frame(
+                (*codec_context).sample_fmt,
+                &(*codec_context).ch_layout,
+                (*codec_context).sample_rate,
+                nb_samples,
+            );
+            let tmp_frame = alloc_audio_frame(
+                AVSampleFormat::AV_SAMPLE_FMT_S16,
+                &(*codec_context).ch_layout,
+                (*codec_context).sample_rate,
+                nb_samples,
+            );
+
+            if ffmpeg::avcodec_parameters_from_context((*stream.as_ptr()).codecpar, codec_context)
+                < 0
+            {
+                panic!("Could not copy the stream parameters");
+            }
+
+            let swr_ctx = ffmpeg::swr_alloc();
+            if swr_ctx.is_null() {
+                panic!("Could not allocate resampler context");
+            }
+
+            ffmpeg::av_opt_set_chlayout(
+                swr_ctx as *mut c_void,
+                c"in_chlayout".as_ptr(),
+                &(*codec_context).ch_layout,
+                0,
+            );
+            ffmpeg::av_opt_set_int(
+                swr_ctx as *mut c_void,
+                c"in_sample_rate".as_ptr(),
+                (*codec_context).sample_rate as i64,
+                0,
+            );
+            ffmpeg::av_opt_set_sample_fmt(
+                swr_ctx as *mut c_void,
+                c"in_sample_fmt".as_ptr(),
+                AVSampleFormat::AV_SAMPLE_FMT_S16,
+                0,
+            );
+            ffmpeg::av_opt_set_chlayout(
+                swr_ctx as *mut c_void,
+                c"out_chlayout".as_ptr(),
+                &(*codec_context).ch_layout,
+                0,
+            );
+            ffmpeg::av_opt_set_int(
+                swr_ctx as *mut c_void,
+                c"out_sample_rate".as_ptr(),
+                (*codec_context).sample_rate as i64,
+                0,
+            );
+            ffmpeg::av_opt_set_sample_fmt(
+                swr_ctx as *mut c_void,
+                c"out_sample_fmt".as_ptr(),
+                (*codec_context).sample_fmt,
+                0,
+            );
+
+            if ffmpeg::swr_init(swr_ctx) < 0 {
+                panic!("Failed to initialize the resampling context");
+            }
+
+            AudioOutputStream {
+                stream: stream.as_ptr(),
+                codec_context,
+                tmp_pkt,
+                frame,
+                tmp_frame,
+                swr_ctx,
+            }
+        }
+    }
+
+    pub fn new2(path: impl AsRef<Path>) {
+        let path = path.as_ref().to_str().unwrap();
+        let path = CString::new(path).unwrap();
+        let format_context = Self::new_output_context(&path);
+
+        let output_format = unsafe { (*format_context.as_ptr()).oformat };
+
+        let video_stream = Self::new_video_streams(format_context, output_format);
+        let audio_stream = Self::new_audio_streams(format_context, output_format);
+
+        unsafe {
+            ffmpeg::av_dump_format(format_context.as_ptr(), 0, path.as_ptr(), 1);
+        }
+    }
+
     pub fn new(
         path: impl AsRef<Path>,
         src_width: usize,

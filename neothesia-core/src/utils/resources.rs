02be3a436a3d54be0ccb5b7d05d599306906f2cf
Bartłomiej Maryńczak <marynczakbartlomiej@gmail.com>
2026-06-30 03:08:01 +0200

    chore: Port from `objc` to `objc2` (#391)

--- neothesia-core/src/utils/resources.rs
+++ neothesia-core/src/utils/resources.rs
@@ -62,38 +62,12 @@ pub fn settings_ron() -> Option<PathBuf> {
 
 #[cfg(target_os = "macos")]
 fn bundled_resource_path(name: &str, extension: &str) -> Option<String> {
-    use objc::{
-        msg_send,
-        runtime::{Class, Object},
-        sel, sel_impl,
-    };
+    use objc2_foundation::{NSBundle, NSString};
 
-    unsafe {
-        let cls = Class::get("NSBundle").unwrap();
-        let bundle: *mut Object = msg_send![cls, mainBundle];
-        let cls = Class::get("NSString").unwrap();
-        let objc_str: *mut Object = msg_send![cls, alloc];
-        let objc_name: *mut Object = msg_send![objc_str,
-                                              initWithBytes:name.as_ptr()
-                                              length:name.len()
-                                              encoding: 4]; // UTF8_ENCODING
-        let objc_str: *mut Object = msg_send![cls, alloc];
-        let objc_ext: *mut Object = msg_send![objc_str,
-                                              initWithBytes:extension.as_ptr()
-                                              length:extension.len()
-                                              encoding: 4]; // UTF8_ENCODING
-        let ini: *mut Object = msg_send![bundle,
-                                         pathForResource:objc_name
-                                         ofType:objc_ext];
-        let _: () = msg_send![objc_name, release];
-        let _: () = msg_send![objc_ext, release];
-        let cstr: *const i8 = msg_send![ini, UTF8String];
-        if cstr != std::ptr::null() {
-            let rstr = std::ffi::CStr::from_ptr(cstr)
-                .to_string_lossy()
-                .into_owned();
-            return Some(rstr);
-        }
-        None
-    }
+    let bundle = NSBundle::mainBundle();
+    let name = NSString::from_str(name);
+    let ext = NSString::from_str(extension);
+    let path = bundle.pathForResource_ofType(Some(&name), Some(&ext))?;
+
+    Some(path.to_string())
 }

3636f2c22389b55c2ac8c33d3885f8def5d19ad3
ورد <kasimeka@tuta.io>
2026-08-16 18:16:52 +0300

    feat(nix): add cli package and devshell prompt prefix (#421)

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  nixConfig.bash-prompt-prefix = "(neothesia) ";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
@@ -28,11 +30,31 @@
           overlays = [inputs.rust-overlay.overlays.default];
         };
       in
-        f system pkgs);
+        f pkgs.stdenv.hostPlatform.system pkgs);
 
     meta = (inputs.nixpkgs.lib.importTOML ./neothesia/Cargo.toml).package;
+
+    workspace = inputs.nixpkgs.lib.cleanSourceWith {
+      name = "${meta.name}-${meta.version}-src";
+      src = ./.;
+      filter = inputs.gitignore.lib.gitignoreFilterWith {
+        basePath = ./.;
+        extraRules =
+          # gitignore
+          ''
+            flake.*
+            LICENSE.md
+            README.md
+            .github
+            makefile
+            *.sf2
+            *.mid
+            docs
+          '';
+      };
+    };
   in {
-    packages = forAllSystems (_system: pkgs: let
+    packages = forAllSystems (system: pkgs: let
       rustToolchain =
         pkgs.rust-bin.nightly.latest.default.override
         {extensions = ["rust-src" "rust-analyzer"];};
@@ -43,34 +65,31 @@
         clippy = rustToolchain;
       };
     in {
-      default = naersk.buildPackage {
+      # `nix run github:PolyMeilex/Neothesia`
+      default = inputs.self.outputs.packages.${system}.neothesia;
+      neothesia = naersk.buildPackage {
         meta.mainProgram = "neothesia";
-        src = pkgs.lib.cleanSourceWith {
-          name = "${meta.name}-${meta.version}-src";
-          src = ./.;
-          filter = inputs.gitignore.lib.gitignoreFilterWith {
-            basePath = ./.;
-            extraRules =
-              # gitignore
-              ''
-                flake.*
-                LICENSE.md
-                README.md
-                .github
-                makefile
-                *.sf2
-                *.mid
-                docs
-              '';
-          };
-        };
+        src = workspace;
+      };
+
+      # `nix run github:PolyMeilex/Neothesia#cli`
+      cli = naersk.buildPackage {
+        cargoBuildOptions = prev: prev ++ ["--package" "neothesia-cli"];
+        nativeBuildInputs = [
+          pkgs.pkg-config
+          pkgs.ffmpeg_8.dev
+        ];
+        meta.mainProgram = "neothesia-cli";
+        src = workspace;
       };
     });
 
     devShells = forAllSystems (system: pkgs: {
-      default =
-        pkgs.mkShell
-        {inputsFrom = [self.packages.${system}.default];};
+      # `nix develop github:PolyMeilex/Neothesia`
+      default = pkgs.mkShell {
+        # grab all build dependencies of all exposed packages
+        inputsFrom = pkgs.lib.attrValues inputs.self.packages.${system};
+      };
     });
   };
 }
